28602ff52efe59c702c44dd8c7247d4ec2f77592
larmic <mail@larmic.de>
2023-09-26 15:26:35 +0200

    move handlers to routers

--- internal/routers/router.go
+++ internal/routers/router.go
@@ -2,7 +2,7 @@ package routers
 
 import (
 	"github.com/gin-gonic/gin"
-	"larmic/golang-starter/internal/handlers"
+	"larmic/golang-starter/internal/routers/handlers"
 )
 
 func InitRouter(externalUrl string) *gin.Engine {
